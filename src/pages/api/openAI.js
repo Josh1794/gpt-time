8f209ddbb821623a935ab25a25f77b386da343e0
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2023-03-17 11:02:10 -0400

    adding generated code

--- src/pages/api/openAI.js
+++ src/pages/api/openAI.js
@@ -1,7 +1,26 @@
-import { Configuration, OpenAIApi } from 'openai';
-const configuration = new Configuration({
-  organization: 'org-cG8v7KHm9fZXWN8NkF4FvXZK',
-  apiKey: process.env.API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+const axios = require('axios');
+
+const prompt = 'Hello, how are you?';
+const url = 'https://api.openai.com/v1/engines/<engine-id>/completions';
+const data = {
+  prompt: prompt,
+  temperature: 0.7,
+  max_tokens: 50,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+  stop: '\n',
+};
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${process.env.API_KEY}`,
+};
+
+axios
+  .post(url, data, { headers: headers })
+  .then((response) => {
+    console.log(response.data.choices[0].text);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
